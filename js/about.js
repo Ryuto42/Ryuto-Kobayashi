14f6165e8a48c6bca6424ac5da115a17dd78fdf7
Ryuto <koba.ryu0402@gmail.com>
2021-08-30 21:17:01 +0900

    page animation

--- js/about.js
+++ js/about.js
@@ -11,3 +11,26 @@ $(window).scroll(function(){
  });
 });
 
+
+$(function() {
+    $('a:not([href^="#"]):not([target])').on('click', function(e){
+        e.preventDefault();
+        url = $(this).attr('href');
+        if (url !== '') {
+            $('body').addClass('is_remove');
+            setTimeout(function () {
+                window.location = url;
+            }, 1000);
+        }
+        return false;
+    });
+  });
+  
+  var fixLoad = function(){
+    document.body.classList.remove("is_fade");
+    document.body.classList.remove("is_remove");
+  } 
+  history.replaceState(null, document.getElementsByTagName('title')[0].innerHTML, null);
+  window.addEventListener('popstate', function() {
+    fixLoad();
+  }, false);
