42081ee6babde90ea1db815cfc8aee220a09d549
Ryuto <koba.ryu0402@gmail.com>
2021-10-23 23:33:14 +0900

    works

--- js/works.js
+++ js/works.js
@@ -1,12 +1,12 @@
-$('.animation').css('visibility','hidden');
+$('.works-element').css('visibility','hidden');
 $(window).scroll(function(){
  var windowHeight = $(window).height(),
      topWindow = $(window).scrollTop();
- $('.animation').each(function(){
+ $('.works-element').each(function(){
   var targetPosition = $(this).offset().top;
    if(topWindow > targetPosition - windowHeight + 30){
     //ここで表示時間の変化できる
-   $(this).addClass("fadeInRight");
+   $(this).addClass("fadeIn");
   }
  });
 });
